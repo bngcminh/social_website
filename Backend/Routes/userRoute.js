454e2d3708c36f07ec4c658a3b30b45bbb220de8
bngcminh <buingocminh2k4@gmail.com>
2026-05-19 16:23:28 +0700

    chỉnh sửa hàm tạo bài viết, thêm hàm sửa và xóa bài viết cho người dùng

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -3,4 +3,6 @@ import { authentication } from '../Middlewares/authentication.js';
 
 export function userRoute(fastify, options){
     fastify.post('/user/create_post', { preHandler: authentication }, userController.createPost);
+    fastify.put('/user/edit_post/:postId', { preHandler: authentication }, userController.editPost);
+    fastify.delete('/user/delete_post/:postId', { preHandler: authentication }, userController.deletePost);
 }
