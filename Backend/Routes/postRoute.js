3c77ba08538c716a748ee9d8dd581e65140bbb53
bngcminh <buingocminh2k4@gmail.com>
2026-05-24 14:24:58 +0700

    chuyển chức năng tạo sửa xóa bài viết sang 1 controller riêng

---
+++ Backend/Routes/postRoute.js
@@ -0,0 +1,7 @@
+import * as postController from '../Controllers/postController.js';
+
+export function postRoute(fastify, option){
+    fastify.post('/create_post', { preHandler: authentication }, postController.createPost);
+    fastify.put('/edit_post/:postId', { preHandler: authentication }, postController.editPost);
+    fastify.delete('/delete_post/:postId', { preHandler: authentication }, postController.deletePost);
+}
