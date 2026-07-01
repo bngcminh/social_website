696ac799d89df5a155506c4f1f190bca2ca7baa8
bngcminh <buingocminh2k4@gmail.com>
2026-07-01 21:56:21 +0700

    xóa hàm thừa

--- Backend/Routes/interactRoute.js
+++ Backend/Routes/interactRoute.js
@@ -7,5 +7,4 @@ export function interactRoute(fastify, option){
     fastify.post('/posts/:postId/comments', { preHandler: authentication }, interactController.createComment);
     fastify.put('/comments/:commentId', { preHandler: authentication }, interactController.editComment);
     fastify.delete('/comments/:commentId', { preHandler: authentication }, interactController.deleteComment);
-    fastify.post('/posts/:postId/repost', { preHandler: authentication }, interactController.rePost);
 }
